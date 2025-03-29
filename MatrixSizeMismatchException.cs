4e0c5ede856eb2def551472a0403ebebc8535d97
Виталий <148930900+Luxeny@users.noreply.github.com>
2025-03-29 16:55:47 +0700

    Comment removed.

--- MatrixSizeMismatchException.cs
+++ MatrixSizeMismatchException.cs
@@ -1,6 +1,5 @@
 using System;
 
-// Исключение для несовпадения размеров матриц
 public class MatrixSizeMismatchException : Exception
 {
     public MatrixSizeMismatchException() : base("Размеры матриц не совпадают.") { }
